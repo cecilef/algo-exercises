fd31e9b18628378955ff109e75365543dfb75026
Cécile Ferrier <cecile@macbook-pro.local>
2018-03-20 21:41:39 +0000

    Fixed error with test

--- src/app/knapsack-problem/knapsack-problem.component.ts
+++ src/app/knapsack-problem/knapsack-problem.component.ts
@@ -17,14 +17,10 @@ export class KnapsackProblemComponent implements OnInit {
     if (capacity === undefined || capacity === 0) return 0;
     if (values.length !== weights.length) return 0;
 
-    let weight = 0;
+    let weight = weights[0];
     let sum = 0;
     for (let i = 0;  i < values.length ; i++) {
-      if (weight + weights[i] <= capacity) {
-        weight += weights[i];
-        sum += values[i];
-      }
-      for (let j = 0; j < values.length ; j++) {
+      for (let j = 0; j < weights.length ; j++) {
         if (i !== j) {
           if (weight + weights[j] <= capacity) {
             weight += weights[j];
